f3403f0d77fadfe73ce51d61b6b6fbc2f5936096
adripleitez <00067218@uca.edu.sv>
2019-11-23 17:20:04 -0600

    fixing links

--- src/utils/apiUrlBase.js
+++ src/utils/apiUrlBase.js
@@ -1,6 +1,9 @@
-let host = "localhost";
-let port = "3500";
-let API = {
-    baseURL: `http://${host}:${port}`}
+// let host = "localhost";
+// let port = "3500";
+// let API = {
+//     baseURL: `http://${host}:${port}`}
 
+let API = {
+    baseURL: ' https://git.heroku.com/restful-api-aspp.git'
+}
 export default API;
